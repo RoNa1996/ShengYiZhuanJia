d0df1df431161680e15d7e0b3cdf4e4461a9ba20
张泽荣 <564146379@qq.com>
2017-09-19 17:20:15 +0800

    实现Login的伪登录功能

--- www/view/login/login.controller.js
+++ www/view/login/login.controller.js
@@ -1,3 +1,28 @@
 /**
  * Created by Administrator on 2017/9/18.
  */
+angular.module('starter.controllers')
+.controller('LoginCtrl',['$scope','$state','$ionicPopup',
+function ($scope,$state,$ionicPopup) {
+
+}]);
+
+$scope.loginData={
+  username:'admin'
+  ,password:'123456'
+};
+
+$scope.login=function () {
+  if($scope.loginData.username=='admin'&&
+    $scope.loginData.password=='123456'){
+  $state.go("app.playlists",{});
+  }
+else {
+$ionicPopup.alert({
+title:'警告',
+  template:'您的用户名或者密码错误',
+  okText:'确定',
+  okType:'button-energized'
+});
+}
+};
