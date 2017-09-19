0e17a5c4aabd02b4d511cd396cb0c1eee9190fc5
张泽荣 <564146379@qq.com>
2017-09-19 16:49:58 +0800

    欢迎页
    完成welcome欢迎页

--- www/js/app.js
+++ www/js/app.js
@@ -32,6 +32,11 @@ angular.module('starter', ['ionic', 'starter.controllers','starter.services'])
     controller: 'AppCtrl'
   })
 
+    .state('welcome', {
+      url: '/welcome',
+      templateUrl: 'view/welcome/welcome.html',
+    })
+
   .state('app.search', {
     url: '/search',
     views: {
@@ -69,5 +74,5 @@ angular.module('starter', ['ionic', 'starter.controllers','starter.services'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+ // $urlRouterProvider.otherwise('/app/playlists');
 });
