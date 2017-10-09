fd0dff2c2b3744c6d96c97fd2dd57eb51a425a52
张泽荣 <564146379@qq.com>
2017-10-09 16:54:41 +0800

    上传商品照片 10月9日

--- www/view/product/product-add.controller.js
+++ www/view/product/product-add.controller.js
@@ -1,28 +1,102 @@
 (function () {
   'use strict';
-  angular.module('starter.controllers').controller('ProductAddCtrl',['$scope','CategoryService',function ($scope,CategoryService) {
-    $scope.product={
-      ID:0,
-      Name:'',
-      CategoryID:CategoryService.activeCategory.ID,
-      Category:{
-        ID:CategoryService.activeCategory.ID,
-        Name:CategoryService.activeCategory.Name
+  angular.module('starter.controllers').controller('ProductAddCtrl',['$scope','CategoryService','$cordovaBarcodeScanner','$cordovaCamera','$cordovaImagePicker','$ionicActionSheet',
+    function ($scope,CategoryService,$cordovaBarcodeScanner,$cordovaCamera,$cordovaImagePicker,$ionicActionSheet) {
+      $scope.product={
+        ID:0,
+        Name:'',
+        Barcode:'',
+        Images:['/views/product/img/1.jpg','/views/product/img/2.jpg','/views/product/img/3.jpg'],
+        CategoryID:CategoryService.activeCategory.ID,
+        Category:{
+          ID:CategoryService.activeCategory.ID,
+          Name:CategoryService.activeCategory.Name
+        }
+      };
+      $scope.$on('CategoryUpdate',function (event,data) {
+        console.log(data);
+        $scope.product.CategoryID=CategoryService.activeCategory.ID;
+        $scope.product.Category.ID=CategoryService.activeCategory.ID;
+        $scope.product.Category.Name=CategoryService.activeCategory.Name;
+      });
+      $scope.scanBarcode = function () {
+        $cordovaBarcodeScanner.scan().then(function (data) {
+            $scope.product.Barcode = data.text;
+          },
+          function (error) {
+
+          });
+      };
+      $scope.showActionSheet = function () {
+        $ionicActionSheet.show({
+          buttons:[{text:'拍照'},{text:'从相册中选取'}]
+          ,cancelText:'<b>取消</b>'
+          ,buttonClicked:function (index) {
+            switch (index){
+              case 0:
+                camera();
+                break;
+              case 1:
+                pickImage();
+                break;
+              default:
+            }
+          }
+        });
+      };
+      function pickImage(){
+        var options={
+          maximumImagesCount:3,
+          width:800,
+          height:800,
+          quality:80
+        };
+        $cordovaImagePicker.getPictures(options).then(function(results){
+          for(var i=0;i<results.length;i++){
+            if($scope.product.Images.length<3){
+              $scope.product.Images.push(results[i]);
+            }
+          }
+        },function(error){
+        });
       }
-    };
-    $scope.$on('CategoryUpdate',function (event,data) {
-      $scope.product.CategoryID =CategoryService.activeCategory.ID;
-      $scope.product.Category.ID=CategoryService.activeCategory.ID;
-      $scope.product.Category.Name=CategoryService.activeCategory.Name;
-      //console.log($scope.product);
-    });
+      function camera() {
+        if($scope.product.Images.length>=3){
+          return;
+        }
+        var option = {
+          destinationType: Camera.DestinationType.FILE_URI,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          saveToPhotoAlbum:true,
+          quality:50
+        };
 
-    $scope.scanBarcode=function () {
-      $cordovaBarcodeScanner.scan().then(function (barcodeData) {
-        $scope.productData.
-      })
-    }
-
-
-  }]);
+        $cordovaCamera.getPicture(options).then(function(imageURI){
+          if($scope.product.Images.length<3){
+            $scope.product.Images.push(imageURI);
+          }
+        },function (err) {
+          //error
+        });
+      }
+      $scope.selectSupplier=function(){
+        $scope.supplier.Name="";
+        $scope.supplier.Phone='';
+        $ionicPopup.show({
+          title:'新增供货商',
+          templateUrl:'/views/supplier/supplier-quick-add.html',
+          scope:$scope,
+          button:[{
+            text:'取消',
+            type:'button-outline button-energized'
+          },{
+            text:'确定',
+            type:'button-energized',
+            onTap:function(){
+              $scope.product.Supplier.Name=$scope.supplier.Name;
+            }
+          }],
+        })
+      };
+    }]);
 })();
