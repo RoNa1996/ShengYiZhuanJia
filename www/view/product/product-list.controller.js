72ae948f02e52760e7a5daaa232212e9c815dc90
张泽荣 <564146379@qq.com>
2017-10-10 17:01:37 +0800

    10月10日修改

--- www/view/product/product-list.controller.js
+++ www/view/product/product-list.controller.js
@@ -11,6 +11,10 @@
         $scope.searchMV ={
           content:''
         };
+
+        $scope.hasMore=true;
+        var isLoading = false;
+        var pageIndex = 1;
         $scope.$on('$ionicView.enter', function () {
           $ionicLoading.show({
             template:'<ion-spinner icon="ios" class="spinner-light"></ion-spinner>数据加载中,请稍后...',
@@ -80,5 +84,84 @@
             Name:$scope.searchMV.content
           });
         };
+
+        $scope.doRefresh = function () {
+          pageIndex =1 ;
+          $scope.hasMore =true;
+          $scope.product =[];
+          loadData();
+        };
+        $scope.loadMore=function () {
+          pageIndex++;
+          loadData();
+        };
+        function  loadData() {
+          if(isLoading == true){
+            return;
+          }
+          isLoading =true;
+          $timeout(function () {
+              var  list =[      {
+                ID:1
+                ,Images: ['view/product/img/iPhone7.png']
+                ,Name:'iphone7'
+                ,Price:'￥5388'
+                ,Store: 12
+                ,Barcode: 'liuzhiyongSB'
+              }
+                , {
+                  ID:2
+                  ,Images: ['view/product/img/SONY.png']
+                  ,Name:'SONY XZP'
+                  ,Price:'￥4500'
+                  ,Store: 12
+                  ,Barcode: 'liuzhiyongSB'
+                }
+                , {
+                  ID:3
+                  ,Images: ['view/product/img/U11.png']
+                  ,Name:'HTC U11'
+                  ,Price:'￥5388.599'
+                  ,Store: 12
+                  ,Barcode: 'liuzhiyongSB'
+                }
+                , {
+                  ID:4
+                  ,Images: []
+                  ,Name:'小米 MIX2'
+                  ,Price:'￥3288'
+                  ,Store: 12
+                  ,Barcode: 'liuzhiyongSB'
+                }
+                , {
+                  ID:5
+                  ,Images: []
+                  ,Name:'冲击钻'
+                  ,Price:'￥88'
+                  ,Store: 12
+                  ,Barcode: 'liuzhiyongSB'
+                }
+                , {
+                  ID:6
+                  ,Images: []
+                  ,Name:'￥纯悦矿泉水'
+                  ,Price:'1'
+                  ,Store: 12
+                  ,Barcode: 'liuzhiyongSB'
+                }
+                ,];
+            $scope.products = $scope.products.concat(list);
+            $scope.sourceProducts=angular.copy($scope,product);
+            $scope.$broadcast('scroll.refreshComplete');
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+            $ionicLoading.hide();
+            }
+
+          )
+
+        }
+
+
+
       }]);
 }());
