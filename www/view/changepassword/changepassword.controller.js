99277b4421ead9a5b9b902fccee8c69551012e6b
张泽荣 <564146379@qq.com>
2017-10-16 14:07:43 +0800

    修改密码完成（BUG修复成功）

--- www/view/changepassword/changepassword.controller.js
+++ www/view/changepassword/changepassword.controller.js
@@ -1,23 +1,24 @@
 (function () {
   'use strict';
-  angular.module('starter.controllers').controller('ChangePasswordCtrl',['$scope',' popupService',function ($scope, popupService) {
+  angular.module('starter.controllers').controller('ChangePasswordCtrl',['$scope','popupService',function ($scope,popupService) {
     $scope.user = {
       oldPassword:'',
       password:'',
       confirmPassword:''
-    }
-    $scope.save=function (valid) {
+    };
+    $scope.save = function (valid) {
       if($scope.changePasswordForm.oldPassword.$error.required){
-        popupService.toast('旧密码必填');
+        popupService.toast('jiumimabitian');
         return;
       }
+
       console.log($scope.changePasswordForm.$valid);
       if($scope.changePasswordForm.$valid){
         console.log('save');
-        popupService.toast('修改成功');
+        popupService.toast('dsflsdflsfldl');
       }
 
-    }
+
+    };
   }]);
 })();
-
